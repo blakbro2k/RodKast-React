be489c0b70fd243832a6dca5aded631c21a656f6
Enoch Boateng <boaten01@gmail.com>
2019-06-30 09:48:08 -0400

    Build Player functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './app/App';
 import {name as appName} from './app.json';
 
 import EStyleSheet from 'react-native-extended-stylesheet';
